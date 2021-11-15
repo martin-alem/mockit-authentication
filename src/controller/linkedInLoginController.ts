fcd65da99906859e682da5c7d403e831a3443e86
Martin Alemajoh <alemajohmartin@gmail.com>
2021-11-15 17:45:22 -0500

    fixed cookie issue

--- src/controller/linkedInLoginController.ts
+++ src/controller/linkedInLoginController.ts
@@ -8,7 +8,12 @@ function LinkedInLoginController(req: Request, res: Response, next: NextFunction
     const user = req.body.user;
     const JWT_SECRET: string = process.env.JWT_SECRET!;
     const accessToken = jwt.sign({ user_id: user["_id"] }, JWT_SECRET, { expiresIn: "1h" });
-    res.cookie("_access_token", `${accessToken}`, { expires: new Date(Date.now() + 1 * 3600000), httpOnly: true });
+    res.cookie("_access_token", `${accessToken}`, {
+      expires: new Date(Date.now() + 1 * 3600000),
+      httpOnly: true,
+      // domain: ".mockit.org",
+      sameSite: "lax",
+    });
     res.status(200).json({ status: "success", statusCode: 200, user: user });
   } catch (error: unknown) {
     Logger.log("error", error as Error, import.meta.url);
