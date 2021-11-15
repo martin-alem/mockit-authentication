e84070c02d35945cb9c82a1518198946583cd988
Martin Alemajoh <alemajohmartin@gmail.com>
2021-11-15 05:55:16 -0500

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import express, { Request, Response, Express } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+
+//Cors configuration
+const corsOptions = {
+  origin: true,
+  credentials: true,
+  methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.enable("trust proxy");
+app.use(cors(corsOptions));
+app.use(express.json());
+
+app.all("*", (req: Request, res: Response) => {
+  res.status(404).json({ status: 404, statusText: "fail", message: "The path you are requesting does not exist" });
+});
+
+const PORT: string = process.env.PORT || `3000`;
+app.listen(PORT, () => {
+  console.log(`Authentication Server Listening On Port ${PORT}`);
+});
