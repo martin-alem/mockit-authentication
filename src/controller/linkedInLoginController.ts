e719c64ab6b0d36484cf8d3548668bcae0a788de
Martin Alemajoh <alemajohmartin@gmail.com>
2021-11-16 17:55:26 -0500

    added create default profile functionality

--- src/controller/linkedInLoginController.ts
+++ src/controller/linkedInLoginController.ts
@@ -6,6 +6,7 @@ import jwt from "jsonwebtoken";
 function LinkedInLoginController(req: Request, res: Response, next: NextFunction): void {
   try {
     const user = req.body.user;
+    const profile = req.body.profile;
     const JWT_SECRET: string = process.env.JWT_SECRET!;
     const accessToken = jwt.sign({ user_id: user["_id"] }, JWT_SECRET, { expiresIn: "1h" });
     res.cookie("_access_token", `${accessToken}`, {
@@ -14,7 +15,7 @@ function LinkedInLoginController(req: Request, res: Response, next: NextFunction
       // domain: ".mockit.org",
       sameSite: "lax",
     });
-    res.status(200).json({ status: "success", statusCode: 200, user: user });
+    res.status(200).json({ status: "success", statusCode: 200, user, profile });
   } catch (error: unknown) {
     Logger.log("error", error as Error, import.meta.url);
     next(new ErrorHandler("Internal server error", 500));
--- src/middleware/checkUserMiddleware.ts
+++ src/middleware/checkUserMiddleware.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from "express";
 import Errorhandler from "./../utils/ErrorHandler.js";
 import Logger from "./../utils/Logger.js";
 import User from "./../model/UserModel.js";
+import ProfileModel from "./../model/ProfileModel.js";
 import { findOne, insertOne, findAndUpdate } from "./../service/query.js";
 
 async function checkUserMiddleware(req: Request, res: Response, next: NextFunction) {
@@ -12,6 +13,16 @@ async function checkUserMiddleware(req: Request, res: Response, next: NextFuncti
     const result = await findOne(User, { emailAddress: emailAddress });
     if (!result) {
       const newUser = await insertOne(User, user);
+      const profileData = {
+        nickName: "",
+        emailAddress: "",
+        languages: [],
+        difficulty: [],
+        userId: newUser._id,
+        lastUpdate: new Date(),
+      };
+      const profile = await insertOne( ProfileModel, profileData );
+      req.body.profile = profile;
       req.body.user = newUser;
       next();
     } else {
